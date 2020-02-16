7bc1efdb3b734cd2d933a2bb45aeb875605967b4
zhenliang.sun <solszl@163.com>
2020-02-17 00:24:46 +0800

    + 视图管理类

---
+++ src/manager/viewManager.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * Created Date: 2020-02-16, 23:34:17 (zhenliang.sun)
+ * Last Modified: 2020-02-16, 23:36:24 (zhenliang.sun)
+ * Email: zhenliang.sun@gmail.com
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ * Copyright (c) 2020 infervision
+ */
+
+class ViewManager {
+  constructor() {}
+}
+
+export default ViewManager
