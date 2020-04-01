f26f35c7a72c798570e5d5685d94e59d831d8e87
zhenliang.sun <solszl@163.com>
2020-04-01 21:49:51 +0800

    + 添加播放功能

--- demo/viewContainer.js
+++ demo/viewContainer.js
@@ -12,11 +12,12 @@ import WWWCCommand from '../src/tools/command/wwwcCommand'
 import ZoomCommand from '../src/tools/command/zoomCommand'
 import ViewContainer from '../src/view/viewContainer'
 import ScrollCommand from '../src/tools/command/scrollCommand'
+import PlaybackCommand from '../src/tools/command/playbackCommand'
 
 /**
  *
  * Created Date: 2020-02-25, 17:32:51 (zhenliang.sun)
- * Last Modified: 2020-04-01, 20:38:55 (zhenliang.sun)
+ * Last Modified: 2020-04-01, 21:49:36 (zhenliang.sun)
  * Email: zhenliang.sun@gmail.com
  *
  * Distributed under the MIT license. See LICENSE file for details.
@@ -26,7 +27,7 @@ import ScrollCommand from '../src/tools/command/scrollCommand'
 log.setDefaultLevel(1)
 
 const urls = []
-for (let i = 1; i < 40; i += 1) {
+for (let i = 1; i < 4; i += 1) {
   urls.push(
     // `http://127.0.0.1:8887/1.2.840.113619.2.278.3.2831172146.890.1539296548.557.${i}`
     `http://127.0.0.1:8887/${i}`
@@ -78,7 +79,7 @@ document.querySelector('#tool-link').addEventListener('click', () => {
   alert(123)
 })
 document.querySelector('#tool-play').addEventListener('click', () => {
-  alert(123)
+  new PlaybackCommand(viewContainer, { framerate: 20 }).execute()
 })
 document.querySelector('#tool-reset').addEventListener('click', () => {
   new ResetCommand(viewContainer).execute()
---
+++ src/tools/command/playbackCommand.js
@@ -0,0 +1,57 @@
+/**
+ *
+ * Created Date: 2020-04-01, 21:13:10 (zhenliang.sun)
+ * Last Modified: 2020-04-01, 21:47:51 (zhenliang.sun)
+ * Email: zhenliang.sun@gmail.com
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ * Copyright (c) 2020 infervision
+ */
+
+import BaseCommand from './baseCommand'
+import log from 'loglevel'
+
+/**
+ * 播放命令， 再次点击就是停止
+ *
+ * @class PlaybackCommand
+ * @extends {BaseCommand}
+ * @author zhenliang.sun
+ */
+class PlaybackCommand extends BaseCommand {
+  constructor(container, config = { framerate: 20 }) {
+    super(container)
+    this._type = 'PlaybackCommand'
+
+    const { framerate } = config
+    this.framerate = framerate || 20
+  }
+
+  execute() {
+    super.execute()
+
+    if ((this.view.playIntervalId || 0) === 0) {
+      this._play()
+    } else {
+      this._stop()
+    }
+  }
+
+  _play() {
+    this.view.playIntervalId = setInterval(() => {
+      const sliceIndex = this.view.sliceIndex.clone()
+      const { slice: total } = this.view.image.geometry.size
+      sliceIndex.k += 1
+      sliceIndex.k %= total
+      sliceIndex.k = Math.max(1, sliceIndex.k)
+      this.view.sliceIndex = sliceIndex
+    }, 1000 / this.framerate)
+  }
+
+  _stop() {
+    clearInterval(this.view.playIntervalId)
+    this.view.playIntervalId = 0
+  }
+}
+
+export default PlaybackCommand
