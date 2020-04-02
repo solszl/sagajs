346cd145a9305762134adec46aa6a542b99562cb
zhenliang.sun <solszl@163.com>
2020-04-02 23:49:35 +0800

    + 添加导出的sdk 对应类内容

---
+++ src/sdk.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * Created Date: 2020-04-02, 23:37:38 (zhenliang.sun)
+ * Last Modified: 2020-04-02, 23:37:58 (zhenliang.sun)
+ * Email: zhenliang.sun@gmail.com
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ * Copyright (c) 2020 infervision
+ */
+
+class SDK {
+  constructor() {}
+}
+
+export default SDK
